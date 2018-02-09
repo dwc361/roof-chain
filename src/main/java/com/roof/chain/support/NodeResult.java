c42f848e4861bc79299e8a8104d265fea3be3832
hzliuxin1 <hzliuxin1@corp.netease.com>
2018-02-09 08:51:30 +0800

    节点返回增加返回数据

--- src/main/java/com/roof/chain/support/NodeResult.java
+++ src/main/java/com/roof/chain/support/NodeResult.java
@@ -3,6 +3,7 @@ package com.roof.chain.support;
 /**
  * 返回结果
  * <p>
+ * next 节点的处理结果（决定下一个流向）
  * state 代表本次执行的状态:
  * 返回结果状态两种表达方式
  * 1.返回字符串
@@ -10,8 +11,12 @@ package com.roof.chain.support;
  * 2.返回码
  * 返回码可以精确的区分各种执行结果  2xx:成功, 4xx:失败, 5xx:错误
  * xx的位数可以根据项目自行定义
+ *
+ * message 对state描述消息
+ *
+ * data 流程处理后返回的结果数据
  */
-public class NodeResult {
+public class NodeResult<T> {
 
     public static final String SUCCESS = "success";
     public static final String FAIL = "fail";
@@ -24,6 +29,7 @@ public class NodeResult {
     private String next;
     private String state;
     private String message;
+    private T data;
 
     public NodeResult(String next) {
         this.state = SUCCESS;
@@ -50,5 +56,13 @@ public class NodeResult {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
     }
 }
