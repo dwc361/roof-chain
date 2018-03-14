a5911b6629871d91ba2c35e35b6c6e6e129525da
hzliuxin1 <hzliuxin1@corp.netease.com>
2018-03-14 15:45:13 +0800

    chain xsd 定义

---
+++ src/main/java/com/roof/chain/config/RoofChainNamespaceHandlerSupport.java
@@ -0,0 +1,14 @@
+package com.roof.chain.config;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * @author hzliuxin1
+ * @since 2018/3/14 0014
+ */
+public class RoofChainNamespaceHandlerSupport extends NamespaceHandlerSupport {
+    @Override
+    public void init() {
+
+    }
+}
